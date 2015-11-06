284341fb17843a78afbee309747742c7ae5d2f13
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-11-06 18:10:56 -0200

    Uploaded

---
+++ run.py
@@ -0,0 +1,111 @@
+#  Ericson's PyTetris
+#
+#  run.py
+#  
+#  Copyright 2015 Ericson Willians (Rederick Deathwill) <EricsonWRP@ERICSONWRP-PC>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+  
+import os
+import pyglet
+import json
+
+LINE_SEGMENT_THICKNESS = 16
+ORIGIN = 0
+
+class Serializable:
+
+	def __init__(self):
+		self.file = None
+		
+	def serialize(self, path, mode):    
+		try:
+			self.file = open(path, mode)
+		except:
+			raise FileNotFoundError()
+		if self.file:
+			return self.file
+
+class App(Serializable, pyglet.window.Window):
+
+	def __init__(self):
+		Serializable.__init__(self)
+		if os.path.isfile("config.txt"):
+			self.data = self.load("config.txt")
+		else:
+			self.data = {
+					"SCREEN_WIDTH": 768,
+					"SCREEN_HEIGHT": 768,
+					"FULLSCREEN": False,
+					"LOCKED_MOUSE": False}
+		self.write("config.txt")
+		pyglet.window.Window.__init__(self, self.data["SCREEN_WIDTH"], 
+											self.data["SCREEN_HEIGHT"], 
+											fullscreen=self.data["FULLSCREEN"])
+		if self.data["LOCKED_MOUSE"]:
+			self.set_exclusive_mouse()
+
+	def write(self, path):
+		self.serialize(path, "w").write(json.dumps(self.data))
+		
+	def load(self, path):
+		json_data = open(path, "r")
+		self.data = json.load(json_data)
+		json_data.close()
+		return self.data
+
+if __name__ == '__main__':
+	app = App()
+	
+	def draw_game_area():
+		pyglet.graphics.draw(24, pyglet.gl.GL_TRIANGLES, 
+			("v2f", (ORIGIN, ORIGIN, ORIGIN, app.height, LINE_SEGMENT_THICKNESS, ORIGIN,
+					 ORIGIN, app.height, LINE_SEGMENT_THICKNESS, app.height, LINE_SEGMENT_THICKNESS, ORIGIN,
+					 ORIGIN, ORIGIN, ORIGIN, LINE_SEGMENT_THICKNESS, app.width / 2, ORIGIN,
+					 ORIGIN, LINE_SEGMENT_THICKNESS, app.width / 2, LINE_SEGMENT_THICKNESS, app.width / 2, ORIGIN,
+					 app.width / 2 - LINE_SEGMENT_THICKNESS, app.height, app.width / 2, app.height, app.width / 2, ORIGIN,
+					 app.width / 2 - LINE_SEGMENT_THICKNESS, app.height, app.width / 2, ORIGIN, app.width / 2 - LINE_SEGMENT_THICKNESS, ORIGIN,
+					 ORIGIN, app.height, app.width / 2, app.height, app.width / 2, app.height - LINE_SEGMENT_THICKNESS,
+					 ORIGIN, app.height, ORIGIN, app.height - LINE_SEGMENT_THICKNESS, app.width / 2, app.height - LINE_SEGMENT_THICKNESS))
+		)
+
+	def draw_texts():
+		pyglet.text.Label(
+			"Ericson's PyTetris - 2015",
+			font_name = "Times New Roman",
+			font_size=16,
+			color = (255, 255, 255, 255),
+			x = app.width - 184, y = app.height - LINE_SEGMENT_THICKNESS,
+			anchor_x = "center", anchor_y = "center"
+		).draw()
+
+	labels = [pyglet.text.Label("0"*8,
+							  font_name = "Times New Roman",
+							  font_size=16,
+							  color = (255, 0, 0, 255),
+							  x = app.width // 2, y = app.height // 2 - n,
+							  anchor_x = "center", anchor_y = "center") for n in range(0, 128, 16)]
+							  
+	@app.event
+	def on_draw():
+		app.clear()
+		#[label.draw() for label in labels]
+		draw_game_area()
+		draw_texts()
+	
+	pyglet.app.run()
